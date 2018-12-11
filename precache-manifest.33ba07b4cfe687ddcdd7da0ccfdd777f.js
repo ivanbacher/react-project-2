81e8b40014edbc29013e7976cae8b4a988387e91
ivan <ivanbacher@users.noreply.github.com>
2018-12-11 10:18:30 +0000

    Updates

---
+++ precache-manifest.33ba07b4cfe687ddcdd7da0ccfdd777f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-project-2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b93e156543a349d0a1b3",
+    "url": "/react-project-2/static/js/runtime~main.b93e1565.js"
+  },
+  {
+    "revision": "cda6408a1cc901743983",
+    "url": "/react-project-2/static/js/main.cda6408a.chunk.js"
+  },
+  {
+    "revision": "f3f6e038f811f5cd4e39",
+    "url": "/react-project-2/static/js/1.f3f6e038.chunk.js"
+  },
+  {
+    "revision": "cda6408a1cc901743983",
+    "url": "/react-project-2/static/css/main.cf6215ea.chunk.css"
+  },
+  {
+    "revision": "f3f6e038f811f5cd4e39",
+    "url": "/react-project-2/static/css/1.46859c04.chunk.css"
+  },
+  {
+    "revision": "be7a5be195f225872ba5869c12f3f120",
+    "url": "/react-project-2/index.html"
+  }
+];
